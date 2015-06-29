0a74403665d86ddb53fd06f4c3585488fa810440
Mashrien <mashrien@gmail.com>
2015-06-29 12:42:20 -0700

    moar

--- DeathAnnounce/DeathAnnounce/DeathAnnounce.cs
+++ DeathAnnounce/DeathAnnounce/DeathAnnounce.cs
@@ -66,7 +66,7 @@ namespace Rocket.Mash.DeathAnnounce {
 
             Msg = DAMsg.CMessage.Replace("{0}", Name);
 
-            Say($"{player.CharacterName} ", DAMsg.CColor);
+            Say($"{player.CharacterName} {Msg}", DAMsg.CColor);
             
             }
 
