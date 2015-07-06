e9ee1af3e4c21b7b0e75400935633fdf8a35c480
Mashrien <mashrien@gmail.com>
2015-07-06 03:01:56 -0700

    asdf

--- GearUp/GearUp/Kit.cs
+++ GearUp/GearUp/Kit.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Rocket.Mash.GearUp {
     public class Kit {
+        [XmlAttribute("KitName")]
+        public string Name;
         public List<Gear> Equipment;
         }
     }
